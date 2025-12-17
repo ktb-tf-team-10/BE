3069cb4dac8c177125585006e0b1d18d63afae59
Eeeegarden <106652275+Eeeegarden@users.noreply.github.com>
2025-12-17 16:06:30 +0900

    initial commit

---
+++ src/main/java/com/ktb_tf_team_10_be/KtbTfTeam10BeApplication.java
@@ -0,0 +1,13 @@
+package com.ktb_tf_team_10_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtbTfTeam10BeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KtbTfTeam10BeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ktb_tf_team_10_be/KtbTfTeam10BeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ktb_tf_team_10_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtbTfTeam10BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
