19acd4bcab822ca9539be28ecf7141a83bc8bcca
Eeeegarden <106652275+Eeeegarden@users.noreply.github.com>
2025-12-18 18:07:50 +0900

    fix: webclient_base_url

--- src/main/java/com/ktb_tf_team_10_be/config/WebClientConfig.java
+++ src/main/java/com/ktb_tf_team_10_be/config/WebClientConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
 
-    private final String baseURL = "http://localhost:8080";
+    private final String baseURL = "http://10.0.0.88:8000";
 
     @Bean
     public WebClient fastApiClient() {
